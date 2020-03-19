1ab43077c3cbdfda043a3f51077f8b8e19e83b0d
Nohemi Tarango <Nohemi5270@gmail.com>
2020-03-19 15:00:43 -0600

    wrote the methods for the author table

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -31,13 +31,14 @@ class Author {
 	 * the username of the author; the name registered with the website
 	 **/
 	private $authorUsername;
+
 	/**
 	 * accessor method for author id
 	 *
 	 * @return int value of author id
 	 **/
 	public function getAuthorId() {
-		return($this->authorId);
+		return ($this->authorId);
 	}
 
 	/**
@@ -48,13 +49,101 @@ class Author {
 	 **/
 	public function setAuthorId($newAuthorId) {
 		// verify the author id is valid
-		$newAuthorId = filter_var ($newAuthorId, FILTER_VALIDATE_INT);
+		$newAuthorId = filter_var($newAuthorId, FILTER_VALIDATE_INT);
 		if($newAuthorId === false) {
 			throw(new UnexpectedValueException("author id is not a valid integar"));
 		}
 
 		// convert and store the author id
 		$this->authorId = intval($newAuthorId);
+	}
+
+	/**
+	 * mutator method for author activation token
+	 *
+	 * @param string $newAuthorActivationToken new value of the author activation token
+	 * @throws UnexpectedValueException if $newAuthorActivationToken is not valid
+	 **/
+	public function setAuthorActivationToken($newAuthorActivationToken) {
+		// verify the author activation token is valid
+		$newAuthorActivationToken = filter_var($newAuthorActivationToken, FILTER_SANITIZE_STRING);
+		if($newAuthorActivationToken === false) {
+			throw(new UnexpectedValueException("author activation token is not a valid string"));
+		}
+
+		// store the author activation token
+		$this->authorActivationToken = $newAuthorActivationToken;
+	}
+
+	/**
+	 * mutator method for author avatar url
+	 *
+	 * @param string $newAuthorAvatarUrl
+	 * @throws UnexpectedValueException if $newAuthorAvatarUrl is not valid
+	 **/
+	public function setAuthorAvatarUrl($newAuthorAvatarUrl) {
+		//verify the author avatar url is valid
+		$newAuthorAvatarUrl = filter_var($newAuthorAvatarUrl, FILTER_SANITIZE_STRING);
+		if($newAuthorAvatarUrl === false) {
+			throw(new UnexpectedValueException("author avatar url is not a valid string"));
+		}
+
+// store the author avatar url
+		$this->authorAvararUrl = $newAuthorAvatarUrl;
+}
+
+
+/**
+ * mutator method for the author email
+ *
+ * @param string $newAuthorEmail new value of email
+ * @throws UnexpectedValueException if $newAuthorEmail is not valid
+ **/
+	public function setAuthorEmail($newAuthorEmail) {
+		//verify the author email is valid
+		$newAuthorEmail = filter_var($newAuthorEmail, FILTER_SANITIZE_STRING);
+		if($newAuthorEmail === false) {
+			throw(new UnexpectedValueException("author email is not a valid string"));
+		}
+
+		// store the author email
+		$this->authorEmail = $newAuthorEmail;
+}
+
+
+/**
+ * mutator method for the author hash
+ *
+ * @param string $newAuthorHash new value of hash
+ * @throws UnexpectedValueException if $newAuthorHash is not valid
+ **/
+	public function setAuthorHash($newAuthorHash) {
+		//verify the author hash is valid
+		$newAuthorHash = filter_var($newAuthorHash, FILTER_SANITIZE_STRING);
+		if($newAuthorHash === false) {
+			throw(new UnexpectedValueException("author hash is not a valid string"));
+		}
+
+		// store the author hash
+		$this->authorHash = $newAuthorHash;
+	}
+
+
+/**
+ * mutator method for the author username
+ *
+ * @param string $newAuthorUsername new value of username
+ * @throws UnexpectedValueException if $newAuthorUsername is not valid
+ **/
+	public function setAuthorUsername($newAuthorUsername) {
+		//verify the author username is valid
+		$newAuthorUsername = filter_var($newAuthorUsername, FILTER_SANITIZE_STRING);
+		if($newAuthorUsername === false) {
+			throw(new UnexpectedValueException("author username is not a valid string"));
+		}
+
+		// store the author username
+		$this->authorUsername = $newAuthorUsername;
 	}
 }
 ?>
